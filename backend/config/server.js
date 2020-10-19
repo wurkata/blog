e38558ee130384f94fa98becc1bb6ec26fd62c64
Yavor Edipov <yavoredipov@gmail.com>
2020-10-19 20:45:48 +0100

    Finish off k8s resources spec

--- backend/config/server.js
+++ backend/config/server.js
@@ -1,5 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  production: true,
   url: "http://0.0.0.0:1337",
+  admin: {
+    url: "/cms",
+  },
 });
