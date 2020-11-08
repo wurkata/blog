46e113e6b93558d8a2eb3ac7a518528da7f426fe
Yavor Edipov <yavoredipov@gmail.com>
2020-11-08 11:30:24 +0000

    Init commit

---
+++ frontend/src/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './containers/App';
+import client from './utils/apolloClient';
+import './styles/core.scss';
+
+ReactDOM.render(
+  <Router>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </Router>,
+  document.getElementById('root'),
+);
